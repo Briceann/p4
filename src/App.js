80fa017e21d3fc8f68d45a310ff4af774e11247b
Thelma Wendy <thelmawendy@Mac.lan>
2024-12-08 14:13:45 -0600

    modified the app to include my name, course and success message

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello Madison WI!!!!</h1>
+      <h1>Brice Aneh's Cloud Project 4 successfully deploying!!!</h1>
     </div>
   );
 }
